dcaf38613272174d463db7fb7a73a597778bc35b
Parker <pp101socials@gmail.com>
2023-09-27 09:20:04 -0400

    Created arrays

--- assets/data/variables/ARRAYS.js
+++ assets/data/variables/ARRAYS.js
@@ -1,1 +1,62 @@
 // This file is used to hold some arrays
+
+const TITLES = [
+  "Elty's Premium Painting & Restoration | Expert House Painting, Popcorn Ceiling Removal, and Restoration Services in Mt. Airy & Greensboro, NC",
+];
+
+const DESCRIPTIONS = [
+  "Elty's Premium Painting & Restoration is your trusted expert for house painting, popcorn ceiling removal, skim coating, staining, sheetrock repair, and pressure washing in Mt. Airy and Greensboro, NC. With over a decade of experience, our top-rated painting contractors specialize in delivering high-quality interior and exterior painting services. From wood staining to sheetrock repair, we are your local painting company in North Carolina committed to excellence. Contact us for professional painting and restoration services that exceed your expectations.",
+];
+
+const INDEX_KWS = [
+  // Company-Specific Keywords
+  "Elty's Premium Painting & Restoration",
+  "Elty's painting services NC",
+  "North Carolina house restoration by Elty's",
+  "Elty's Premium Painting Greensboro",
+  "Elty's Greensboro house painters",
+
+  // House Painting Keywords
+  "House painting services in Greensboro NC",
+  "Residential painters near Mt. Airy NC",
+  "Experienced house painters in North Carolina",
+  "Interior and exterior painting specialists",
+
+  // Popcorn Ceilings Keywords
+  "Popcorn ceiling removal experts in Greensboro NC",
+  "Professional ceiling texture repair in Mt. Airy NC",
+  "Ceiling renovation services in North Carolina",
+
+  // Skim Coating Keywords
+  "Skim coating services in Greensboro NC",
+  "Smooth wall finish experts near Mt. Airy NC",
+  "Wall texture repair in North Carolina",
+
+  // Staining Keywords
+  "Wood staining in Mt. Airy NC",
+  "Deck staining specialists in Greensboro NC",
+  "Exterior and interior wood finishing in North Carolina",
+
+  // Sheetrock Repair Keywords
+  "Sheetrock repair in Greensboro NC",
+  "Drywall repair near Mt. Airy NC",
+  "Professional wall repair services in North Carolina",
+
+  // Pressure-Washing Keywords
+  "Pressure washing services in North Carolina",
+  "Exterior cleaning in Greensboro NC",
+  "Power washing specialists near Mt. Airy NC",
+
+  // Location-Specific Keywords
+  "Painting services in Mt. Airy, North Carolina",
+  "Greensboro painting and restoration",
+  "Local painting company in North Carolina",
+
+  // General Painting Contractor Keywords
+  "Professional painting and restoration in North Carolina",
+  "Top-rated painting contractors in Greensboro NC",
+  "Quality house painting services in Mt. Airy NC",
+  "Experienced painting and repair team in North Carolina",
+];
+
+export { TITLES, INDEX_KWS, DESCRIPTIONS };
