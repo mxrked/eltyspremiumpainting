da7944b2540c5f751295a7e96d80df67b9704e99
Parker <pp101socials@gmail.com>
2023-09-27 10:27:21 -0400

    Created file

--- assets/animations/components/FadeRight.js
+++ assets/animations/components/FadeRight.js
@@ -6,8 +6,44 @@
 
 import { motion } from "framer-motion";
 
-import { FADE_RIGHT } from "../objects/FADES";
+// import { FADE_RIGHT } from "../objects/FADES";
 
-export const FadeRight = ({ children }) => {
-  return <motion.div {...FADE_RIGHT}>{children}</motion.div>;
+export const FadeRight = ({ threshold = 0.5, children }) => {
+  const [HAS_ANIMATED, SET_HAS_ANIMATED] = useState(false);
+  const CONTROLS = useAnimation();
+  const REF = useRef();
+
+  useEffect(() => {
+    const ELEMENT = REF.current;
+
+    const handleScroll = () => {
+      // Making it only display once
+      if (!HAS_ANIMATED) {
+        // Getting if the user has scrolled close to the element
+        const { TOP, BOTTOM } = ELEMENT.getBoundingClientRect();
+        const IS_VISIBLE = TOP < window.innerHeight * threshold && BOTTOM >= 0;
+
+        // Displaying element
+        if (IS_VISIBLE) {
+          CONTROLS.start({ opacity: 1, x: 0 });
+          SET_HAS_ANIMATED(true);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [CONTROLS, threshold, HAS_ANIMATED]);
+
+  return (
+    <motion.div
+      ref={REF}
+      initial={{ opacity: 0, x: 50 }}
+      animate={CONTROLS}
+      transition={{ duration: 0.5 }}
+    >
+      {children}
+    </motion.div>
+  );
 };
--- assets/animations/components/FadeUp.js
+++ assets/animations/components/FadeUp.js
@@ -4,10 +4,47 @@
  *
  */
 
-import { motion } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+import { motion, useAnimation } from "framer-motion";
 
-import { FADE_UP } from "../objects/FADES";
+// import { FADE_UP } from "../objects/FADES";
 
-export const FadeUp = ({ children }) => {
-  return <motion.div {...FADE_UP}>{children}</motion.div>;
+export const FadeUp = ({ threshold = 0.5, children }) => {
+  const [HAS_ANIMATED, SET_HAS_ANIMATED] = useState(false);
+  const CONTROLS = useAnimation();
+  const REF = useRef();
+
+  useEffect(() => {
+    const ELEMENT = REF.current;
+
+    const handleScroll = () => {
+      // Making it only display once
+      if (!HAS_ANIMATED) {
+        // Getting if the user has scrolled close to the element
+        const { TOP, BOTTOM } = ELEMENT.getBoundingClientRect();
+        const IS_VISIBLE = TOP < window.innerHeight * threshold && BOTTOM >= 0;
+
+        // Displaying element
+        if (IS_VISIBLE) {
+          CONTROLS.start({ opacity: 1, y: 0 });
+          SET_HAS_ANIMATED(true);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [CONTROLS, threshold, HAS_ANIMATED]);
+
+  return (
+    <motion.div
+      ref={REF}
+      initial={{ opacity: 0, y: 50 }}
+      animate={CONTROLS}
+      transition={{ duration: 0.5 }}
+    >
+      {children}
+    </motion.div>
+  );
 };
