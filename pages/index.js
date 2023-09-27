8c0d6b0d22a736a87c7eb55872c6dbbbd1138192
Parker <pp101socials@gmail.com>
2023-09-27 09:20:29 -0400

    Added PageHead

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,57 @@
 // React/Next Imports
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import fs from "fs";
+import path from "path";
 
 // Library Imports
 
 // Data/Functions/Images Imports
 
 // Component Imports
+import { PageHead } from "@/assets/components/global/All/PageHead";
 
 // Style Imports
 import "../assets/styles/modules/Index/Index.module.css";
 
-export default function Home() {
+export async function getStaticProps() {
+  try {
+    const filePath = path.join(
+      process.cwd(),
+      "public/data/",
+      "PageHeadIconsData.json"
+    );
+    const fileContents = fs.readFileSync(filePath, "utf8");
+    const iconData = JSON.parse(fileContents);
+
+    return {
+      props: {
+        iconData,
+      },
+    };
+  } catch (error) {
+    console.error("Error reading icon data:", error);
+
+    return {
+      props: {
+        iconData: null,
+      },
+    };
+  }
+}
+
+export default function Home({ iconData }) {
   const router = useRouter();
 
-  return "Home Page";
+  useEffect(() => {
+    console.log(iconData.favicon);
+  }, []);
+
+  return (
+    <div id="PAGE" className="page index-page">
+      <PageHead page_head_data={iconData} />
+
+      <main id="PAGE_CNT" className="page-cnt"></main>
+    </div>
+  );
 }
