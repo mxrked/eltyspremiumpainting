45e6affd47ef2355bc9b1650ff3f2bd7e0dd6c25
Parker <pp101socials@gmail.com>
2023-09-28 12:13:32 -0400

    Created file

--- assets/components/pages/Index/IndexTop.js
+++ assets/components/pages/Index/IndexTop.js
@@ -4,12 +4,51 @@
  *
  */
 
+import { INDEX_TOP_BG } from "@/assets/cdns/CDNBgs";
+
+import { LazyLoadBackgroundImage } from "../../global/All/LazyLoadBackgroundImage";
+
 import styles from "../../../styles/modules/Index/Index.module.css";
+import { FaArrowAltCircleDown } from "react-icons/fa";
 
 export const IndexTop = () => {
   return (
     <section id="indexTop" className={`${styles.index_top}`}>
-      Index Top
+      <LazyLoadBackgroundImage
+        image_alt={
+          "Elty's Premium Painting & Restoration: Two painters working on a purple house outside."
+        }
+        image_url={INDEX_TOP_BG}
+        style_className={styles.index_top_bg}
+      />
+
+      <div className={`${styles.index_top_overlay}`}>
+        <div className={`${styles.index_top_overlay_cnt}`}>
+          <h1 className="orientation-change-element half-second">
+            Expert House Painting and Restoration in Mt. Airy & Greensboro, NC
+          </h1>
+
+          <p className="orientation-change-element half-second">
+            Your Mt. Airy & Greensboro, NC Painting and Restoration Experts. We
+            specialize in removing popcorn ceilings, skim coating, staining,
+            sheetrock repair, and pressure-washing for your home.
+          </p>
+
+          <button
+            className="orientation-change-element half-second"
+            onClick={() => {
+              if (document.getElementById("indexContact")) {
+                console.log("Scrolling to Index Contact");
+                document.getElementById("indexContact").scrollIntoView();
+              }
+            }}
+          >
+            <span>Book A Service</span>
+
+            <FaArrowAltCircleDown className={`${styles.icon}`} />
+          </button>
+        </div>
+      </div>
     </section>
   );
 };
