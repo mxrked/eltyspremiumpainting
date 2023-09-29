210fd208c0ba33efafac45939beb6090100c28ed
Parker <pp101socials@gmail.com>
2023-09-29 07:00:54 -0400

    Fixed scroll issues

--- assets/animations/components/FadeLeft.js
+++ assets/animations/components/FadeLeft.js
@@ -4,37 +4,55 @@
  *
  */
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, useCallback } from "react";
 import { motion, useAnimation } from "framer-motion";
 
-// import { FADE_LEFT } from "../objects/FADES";
+const useVisibility = (threshold) => {
+  // State to store the visibility status of each element
+  const [elementVisibilityMap, setElementVisibilityMap] = useState(new Map());
 
-export const FadeLeft = ({ threshold = 0.5, delay = 0, children }) => {
-  const [HAS_ANIMATED, SET_HAS_ANIMATED] = useState(false);
+  // Callback function to handle scroll events and update element visibility
+  const handleScroll = useCallback(() => {
+    // Get all elements with the specified class
+    const ELEMENTS = document.querySelectorAll(".fm-element");
+
+    // Check the visibility of each element
+    ELEMENTS.forEach((element) => {
+      const { top, height } = element.getBoundingClientRect();
+      // Determine if the element is partially visible based on the threshold
+      const PARTIALLY_VISIBLE = top + height * threshold < window.innerHeight;
+
+      // Update the visibility status in the map
+      setElementVisibilityMap((prevMap) => {
+        const newMap = new Map(prevMap);
+        newMap.set(element, PARTIALLY_VISIBLE);
+        return newMap;
+      });
+    });
+  }, [threshold]);
+
+  // Effect to add and remove the scroll event listener
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [handleScroll]);
+
+  // Function to get the visibility status of a specific element
+  return (element) => elementVisibilityMap.get(element) || false;
+};
+
+export const FadeLeft = ({ threshold = 0.5, children }) => {
+  const IS_VISIBLE = useVisibility(threshold);
   const CONTROLS = useAnimation();
   const REF = useRef();
 
   useEffect(() => {
-    const ELEMENT = REF.current;
-
-    const handleScroll = () => {
-      if (!HAS_ANIMATED && ELEMENT) {
-        const { left, width } = ELEMENT.getBoundingClientRect();
-
-        const IS_VISIBLE =
-          window.scrollX + window.innerWidth > left + width * threshold;
-
-        if (IS_VISIBLE) {
-          CONTROLS.start({ opacity: 1, x: 0 });
-          SET_HAS_ANIMATED(true);
-        }
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [CONTROLS, threshold, HAS_ANIMATED]);
+    if (IS_VISIBLE(REF.current)) {
+      CONTROLS.start({ opacity: 1, x: 0 });
+    }
+  }, [CONTROLS, IS_VISIBLE]);
 
   return (
     <motion.div
--- assets/animations/components/FadeRight.js
+++ assets/animations/components/FadeRight.js
@@ -4,36 +4,55 @@
  *
  */
 
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { motion, useAnimation } from "framer-motion";
 
-// import { FADE_RIGHT } from "../objects/FADES";
+const useVisibility = (threshold) => {
+  // State to store the visibility status of each element
+  const [elementVisibilityMap, setElementVisibilityMap] = useState(new Map());
+
+  // Callback function to handle scroll events and update element visibility
+  const handleScroll = useCallback(() => {
+    // Get all elements with the specified class
+    const ELEMENTS = document.querySelectorAll(".fm-element");
+
+    // Check the visibility of each element
+    ELEMENTS.forEach((element) => {
+      const { top, height } = element.getBoundingClientRect();
+      // Determine if the element is partially visible based on the threshold
+      const PARTIALLY_VISIBLE = top + height * threshold < window.innerHeight;
+
+      // Update the visibility status in the map
+      setElementVisibilityMap((prevMap) => {
+        const newMap = new Map(prevMap);
+        newMap.set(element, PARTIALLY_VISIBLE);
+        return newMap;
+      });
+    });
+  }, [threshold]);
+
+  // Effect to add and remove the scroll event listener
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [handleScroll]);
+
+  // Function to get the visibility status of a specific element
+  return (element) => elementVisibilityMap.get(element) || false;
+};
 
 export const FadeRight = ({ threshold = 0.5, children }) => {
-  const [HAS_ANIMATED, SET_HAS_ANIMATED] = useState(false);
+  const IS_VISIBLE = useVisibility(threshold);
   const CONTROLS = useAnimation();
   const REF = useRef();
 
   useEffect(() => {
-    const ELEMENT = REF.current;
-
-    const handleScroll = () => {
-      if (!HAS_ANIMATED && ELEMENT) {
-        const { left, width } = ELEMENT.getBoundingClientRect();
-        const IS_VISIBLE =
-          window.scrollX + window.innerWidth > left + width * threshold;
-
-        if (IS_VISIBLE) {
-          CONTROLS.start({ opacity: 1, x: 0 });
-          SET_HAS_ANIMATED(true);
-        }
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [CONTROLS, threshold, HAS_ANIMATED]);
+    if (IS_VISIBLE(REF.current)) {
+      CONTROLS.start({ opacity: 1, x: 0 });
+    }
+  }, [CONTROLS, IS_VISIBLE]);
 
   return (
     <motion.div
