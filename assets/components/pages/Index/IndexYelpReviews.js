1e98be526036f54dd38c2db05ff67cf4031e8b16
Parker <pp101socials@gmail.com>
2023-09-30 07:09:40 -0400

    Created file

---
+++ assets/components/pages/Index/IndexYelpReviews.js
@@ -0,0 +1,16 @@
+/**
+ *
+ *  This is the Index Yelp Reviews
+ *
+ */
+
+import styles from "../../../styles/modules/Index/Index.module.css";
+
+export const IndexYelpReviews = (props) => {
+  return (
+    <section
+      id="indexYelpReviews"
+      className={`${styles.index_yelp_reviews}`}
+    ></section>
+  );
+};
