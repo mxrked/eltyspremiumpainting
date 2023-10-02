0a8410ff0892b97a1b4eaf1c3548f8093ec4e4c0
Parker <pp101socials@gmail.com>
2023-10-02 09:21:49 -0400

    Added Google Reviews logo

--- assets/cdns/CDNImgs.js
+++ assets/cdns/CDNImgs.js
@@ -17,8 +17,13 @@ const SPLASH_MAIN_IMG = CDNImgReturn("index", "paint-splash-recolored", "webp");
 const COMBO_1 = CDNImgReturn("index", "combo-1", "webp");
 
 const YELP_LOGO = CDNImgReturn("reviews", "yelp-logo", "webp");
+const GOOGLE_REVIEWS_LOGO = CDNImgReturn(
+  "reviews",
+  "google-reviews-logo",
+  "webp"
+);
 
 export { LOGO };
 export { SPLASH_CORNER_IMG, SPLASH_MAIN_IMG };
 export { COMBO_1 };
-export { YELP_LOGO };
+export { YELP_LOGO, GOOGLE_REVIEWS_LOGO };
