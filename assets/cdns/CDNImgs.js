e2b8e3af5008bc01943124c3d98a5717310567d0
Parker <pp101socials@gmail.com>
2023-10-05 07:57:00 -0400

    Added img

--- assets/cdns/CDNImgs.js
+++ assets/cdns/CDNImgs.js
@@ -15,6 +15,7 @@ const SPLASH_CORNER_IMG = CDNImgReturn(
 );
 const SPLASH_MAIN_IMG = CDNImgReturn("index", "paint-splash-recolored", "webp");
 const COMBO_1 = CDNImgReturn("index", "combo-1", "webp");
+const COMBO_2 = CDNImgReturn("index", "combo-2", "webp");
 
 const YELP_LOGO = CDNImgReturn("reviews", "yelp-logo", "webp");
 const GOOGLE_REVIEWS_LOGO = CDNImgReturn(
@@ -25,5 +26,5 @@ const GOOGLE_REVIEWS_LOGO = CDNImgReturn(
 
 export { LOGO };
 export { SPLASH_CORNER_IMG, SPLASH_MAIN_IMG };
-export { COMBO_1 };
+export { COMBO_1, COMBO_2 };
 export { YELP_LOGO, GOOGLE_REVIEWS_LOGO };
