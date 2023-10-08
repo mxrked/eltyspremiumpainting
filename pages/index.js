c41cab34c218d5d75a6eaa7eafba65018cb19356
Parker <pp101socials@gmail.com>
2023-10-08 10:10:57 -0400

    Added component

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,7 @@ import { DesktopNav } from "@/assets/components/global/Nav/Desktop/DesktopNav";
 import { MobileNav } from "@/assets/components/global/Nav/Mobile/MobileNav";
 import { BackToTop } from "@/assets/components/global/All/BackToTop";
 import { Footer } from "@/assets/components/global/Footer/Footer";
+import { SubmissionSuccessMessage } from "@/assets/components/global/All/SubmissionSuccessMessage";
 
 import { IndexTop } from "@/assets/components/pages/Index/IndexTop";
 import { IndexAbout } from "@/assets/components/pages/Index/IndexAbout";
@@ -113,12 +114,35 @@ export default function Home({ iconData, reviewsData, galleryData }) {
     });
   }, []);
 
+  // Displaying the submission success message if sent successfully
+  useEffect(() => {
+    if (sessionStorage.getItem("Submission Sent")) {
+      document
+        .getElementById("submissionSuccessMessage")
+        .querySelector("button").style.pointerEvents = "auto";
+
+      document.getElementById("submissionSuccessMessage").style.opacity = 1;
+      document.getElementById("submissionSuccessMessage").style.visibility =
+        "visible";
+
+      setTimeout(() => {
+        document.getElementById("submissionSuccessMessage").style.opacity = 0;
+        document.getElementById("submissionSuccessMessage").style.visibility =
+          "hidden";
+        document
+          .getElementById("submissionSuccessMessage")
+          .querySelector("button").style.pointerEvents = "none";
+      }, 7000);
+    }
+  }, []);
+
   return (
     <div id="PAGE" className="page index-page">
       <PageHead page_head_data={iconData} />
       <BackToTop />
       <IndexGalleryImgModal />
       <IndexGalleryVideoModal />
+      <SubmissionSuccessMessage />
 
       <main id="PAGE_CNT" className={`${styles.page_cnt} page-cnt`}>
         <NavTop />
