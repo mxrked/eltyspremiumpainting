0f33d7ce368b5dea7dba0ccd02b1c566915fbf53
Parker <pp101socials@gmail.com>
2023-10-18 06:59:45 -0400

    Created file

--- assets/components/pages/Index/IndexAd.js
+++ assets/components/pages/Index/IndexAd.js
@@ -4,32 +4,50 @@
  *
  */
 
+import { useState, useEffect } from "react";
+
 import { LazyLoadImage } from "react-lazy-load-image-component";
 
 import styles from "../../../styles/modules/Index/Index.module.css";
 
 export const IndexAd = (props) => {
-  const AD_TYPE = "november-2023";
+  const [DETECT_ADS, SET_DETECT_ADS] = useState("");
+
+  // Detecting if there are any ads on the page
+  useEffect(() => {
+    if (props.adData[0]) {
+      SET_DETECT_ADS("true");
+    } else {
+      SET_DETECT_ADS("");
+    }
+  }, [DETECT_ADS]);
 
   return (
-    <section id="indexAd" className={`${styles.index_ad}`}>
-      <div className={`${styles.index_ad_inner}`}>
-        {AD_TYPE === "november-2023" ? (
-          <div className={`${styles.ad}`} key={props.adData[0].adID}>
-            <h3 className="orientation-change-element half-second">
-              {props.adData[0].adText_A}
-              <span> </span>
-              <br />
-              {props.adData[0].adText_B}
-            </h3>
+    <div>
+      {DETECT_ADS !== "" ? (
+        <section id="indexAds" className={`${styles.index_ad}`}>
+          <div className={`${styles.index_ad_inner}`}>
+            {DETECT_ADS === "true" ? (
+              <div className={`${styles.ad}`} key={props.adData[0].adID}>
+                <h3
+                  className="orientation-change-element half-second"
+                  id="indexAds_JUMPPOINT"
+                >
+                  {props.adData[0].adText_A}
+                  <span> </span>
+                  <br />
+                  {props.adData[0].adText_B}
+                </h3>
 
-            <LazyLoadImage
-              src={props.adData[0].adImg}
-              alt={props.adData[0].adAlt}
-            />
+                <LazyLoadImage
+                  src={props.adData[0].adImg}
+                  alt={props.adData[0].adAlt}
+                />
+              </div>
+            ) : null}
           </div>
-        ) : null}
-      </div>
-    </section>
+        </section>
+      ) : null}
+    </div>
   );
 };
