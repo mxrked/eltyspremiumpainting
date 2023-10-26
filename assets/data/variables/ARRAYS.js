b62d569dd14a1a7e68d7562de732315c2561b216
Parker <pp101socials@gmail.com>
2023-10-26 06:26:28 -0400

    Modified keywords

--- assets/data/variables/ARRAYS.js
+++ assets/data/variables/ARRAYS.js
@@ -11,86 +11,139 @@ const DESCRIPTIONS = [
   "Elty's Premium Painting: Expert house painting, popcorn ceiling removal, skim coating, staining, sheetrock repair, and pressure washing in Mt. Airy & Greensboro, NC. With over a decade of experience, we specialize in delivering high-quality interior and exterior painting services. Book a service now!",
 ];
 
+// const OLD_INDEX_KWS = [
+//   // Company-Specific Keywords
+//   "Elty's Premium Painting & Restoration",
+//   "Elty's painting services NC",
+//   "North Carolina house restoration by Elty's",
+//   "Elty's Premium Painting Greensboro",
+//   "Elty's Greensboro house painters",
+//   "Elty's Premium Painting Mt Airy",
+//   "Greensboro house restoration services",
+//   "NC professional painting contractors",
+//   "Best painters in Mt. Airy NC",
+//   "Elty's Greensboro residential painting",
+//   "House restoration near Mt Airy, NC",
+//   "Top-rated painting services Greensboro",
+//   "Expert house painters in North Carolina",
+//   "Elty's Premium Greensboro residential painting",
+//   "Local restoration services Mt. Airy",
+//   "Greensboro exterior house painting",
+//   "Residential painting in North Carolina",
+//   "Elty's Premium Mt. Airy home improvement",
+//   "Painting and restoration services NC",
+//   "Greensboro commercial painting specialists",
+//   "Elty's Premium Painting reviews NC",
+//   "Quality house painting in Mt. Airy",
+//   "Greensboro interior house painters",
+//   "Best restoration company in North Carolina",
+//   "Elty's Premium Painting & Restoration cost",
+//   "House painters Mt. Airy NC",
+//   "Greensboro home renovation and painting",
+//   "Professional painters in NC",
+//   "Elty's Premium Painting contact info",
+//   "Mt. Airy exterior home restoration",
+//   "Greensboro property painting and repair",
+//   "Elty's Premium Painting service areas",
+//   "North Carolina painting and restoration",
+//   "Elty's Greensboro painting contractors",
+//   "Residential restoration Mt. Airy NC",
+//   "Greensboro house painting quotes",
+
+//   // House Painting Keywords
+//   "House painting services in Greensboro NC",
+//   "Residential painters near Mt. Airy NC",
+//   "Experienced house painters in North Carolina",
+//   "Interior and exterior painting specialists",
+
+//   // Popcorn Ceilings Keywords
+//   "Popcorn ceiling removal experts in Greensboro NC",
+//   "Professional ceiling texture repair in Mt. Airy NC",
+//   "Ceiling renovation services in North Carolina",
+
+//   // Skim Coating Keywords
+//   "Skim coating services in Greensboro NC",
+//   "Smooth wall finish experts near Mt. Airy NC",
+//   "Wall texture repair in North Carolina",
+
+//   // Staining Keywords
+//   "Wood staining in Mt. Airy NC",
+//   "Deck staining specialists in Greensboro NC",
+//   "Exterior and interior wood finishing in North Carolina",
+
+//   // Sheetrock Repair Keywords
+//   "Sheetrock repair in Greensboro NC",
+//   "Drywall repair near Mt. Airy NC",
+//   "Professional wall repair services in North Carolina",
+
+//   // Pressure-Washing Keywords
+//   "Pressure washing services in North Carolina",
+//   "Exterior cleaning in Greensboro NC",
+//   "Power washing specialists near Mt. Airy NC",
+
+//   // Location-Specific Keywords
+//   "Painting services in Mt. Airy, North Carolina",
+//   "Greensboro painting and restoration",
+//   "Local painting company in North Carolina",
+
+//   // General Painting Contractor Keywords
+//   "Professional painting and restoration in North Carolina",
+//   "Top-rated painting contractors in Greensboro NC",
+//   "Quality house painting services in Mt. Airy NC",
+//   "Experienced painting and repair team in North Carolina",
+// ];
+
 const INDEX_KWS = [
-  // Company-Specific Keywords
-  "Elty's Premium Painting & Restoration",
-  "Elty's painting services NC",
-  "North Carolina house restoration by Elty's",
-  "Elty's Premium Painting Greensboro",
-  "Elty's Greensboro house painters",
-  "Elty's Premium Painting Mt Airy",
-  "Greensboro house restoration services",
-  "NC professional painting contractors",
-  "Best painters in Mt. Airy NC",
-  "Elty's Greensboro residential painting",
-  "House restoration near Mt Airy, NC",
-  "Top-rated painting services Greensboro",
-  "Expert house painters in North Carolina",
-  "Elty's Premium Greensboro residential painting",
-  "Local restoration services Mt. Airy",
-  "Greensboro exterior house painting",
-  "Residential painting in North Carolina",
-  "Elty's Premium Mt. Airy home improvement",
-  "Painting and restoration services NC",
-  "Greensboro commercial painting specialists",
-  "Elty's Premium Painting reviews NC",
-  "Quality house painting in Mt. Airy",
-  "Greensboro interior house painters",
-  "Best restoration company in North Carolina",
-  "Elty's Premium Painting & Restoration cost",
-  "House painters Mt. Airy NC",
-  "Greensboro home renovation and painting",
-  "Professional painters in NC",
-  "Elty's Premium Painting contact info",
-  "Mt. Airy exterior home restoration",
-  "Greensboro property painting and repair",
-  "Elty's Premium Painting service areas",
-  "North Carolina painting and restoration",
-  "Elty's Greensboro painting contractors",
-  "Residential restoration Mt. Airy NC",
-  "Greensboro house painting quotes",
-
-  // House Painting Keywords
-  "House painting services in Greensboro NC",
-  "Residential painters near Mt. Airy NC",
-  "Experienced house painters in North Carolina",
-  "Interior and exterior painting specialists",
-
-  // Popcorn Ceilings Keywords
-  "Popcorn ceiling removal experts in Greensboro NC",
-  "Professional ceiling texture repair in Mt. Airy NC",
-  "Ceiling renovation services in North Carolina",
-
-  // Skim Coating Keywords
-  "Skim coating services in Greensboro NC",
-  "Smooth wall finish experts near Mt. Airy NC",
-  "Wall texture repair in North Carolina",
-
-  // Staining Keywords
-  "Wood staining in Mt. Airy NC",
-  "Deck staining specialists in Greensboro NC",
-  "Exterior and interior wood finishing in North Carolina",
-
-  // Sheetrock Repair Keywords
-  "Sheetrock repair in Greensboro NC",
-  "Drywall repair near Mt. Airy NC",
-  "Professional wall repair services in North Carolina",
-
-  // Pressure-Washing Keywords
-  "Pressure washing services in North Carolina",
-  "Exterior cleaning in Greensboro NC",
-  "Power washing specialists near Mt. Airy NC",
-
-  // Location-Specific Keywords
-  "Painting services in Mt. Airy, North Carolina",
-  "Greensboro painting and restoration",
-  "Local painting company in North Carolina",
-
-  // General Painting Contractor Keywords
-  "Professional painting and restoration in North Carolina",
-  "Top-rated painting contractors in Greensboro NC",
-  "Quality house painting services in Mt. Airy NC",
-  "Experienced painting and repair team in North Carolina",
+  "Elty's Premium Painting & Restoration in Mt. Airy, NC",
+  "Top House Painters in Greensboro",
+  "Local Painting Company Near Me",
+  "Expert Restoration Services in Mt. Airy & Greensboro",
+  "Interior and Exterior House Painting",
+  "Best Residential Painting in North Carolina",
+  "Affordable House Improvement in Mt. Airy & Greensboro",
+  "Quality Home Painting in Greensboro, NC",
+  "Experienced Painters for North Carolina Homes",
+  "Expert Ceiling and Wall Repairs",
+  "Local Wood Staining Services in Mt. Airy",
+  "Greensboro's Trusted Home Improvement Services",
+  "Full-Service Painting and Restoration",
+  "Local Greensboro Painting Contractors",
+  "Affordable Residential Painting Solutions in Mt. Airy",
+  "Greensboro's Expert Wood Stainers",
+  "Local House Restoration Experts in North Carolina",
+  "Residential Painting and Repair in Greensboro & Mt. Airy",
+  "Affordable Home Painting Services",
+  "Expert Popcorn Ceiling Removal in North Carolina",
+  "High-Quality Skim Coating in Mt. Airy & Greensboro",
+  "Quality Deck Staining Services in Greensboro, NC",
+  "Professional Painting and Repair Services",
+  "Affordable House Improvement Solutions in Mt. Airy & Greensboro",
+  "Local Popcorn Ceiling Removal Specialists in North Carolina",
+  "Greensboro's Preferred Skim Coating Contractors",
+  "Full-Service Deck Staining in Mt. Airy",
+  "Professional House Painting and Restoration in North Carolina",
+  "Affordable Home Improvement Services in Greensboro, NC",
+  "Greensboro's Leading Popcorn Ceiling Removal Services",
+  "Top Skim Coating Experts in Mt. Airy & Greensboro",
+  "Quality Home Deck Staining in North Carolina",
+  "Affordable Painting and Repair Services",
+  "Experienced House Improvement in Mt. Airy & Greensboro",
+  "Expert Popcorn Ceiling Removal in Greensboro, NC",
+  "Greensboro's Skim Coating Specialists",
+  "Residential Deck Staining in Mt. Airy",
+  "Trusted Painting and Restoration in North Carolina",
+  "Greensboro's Affordable Home Improvement Services",
+  "Local Popcorn Ceiling Removal Experts in Mt. Airy & Greensboro",
+  "Skim Coating Solutions for North Carolina Homes",
+  "Greensboro's Quality Deck Staining Services",
+  "Residential Painting and Restoration Experts",
+  "Affordable House Painting and Repair in Mt. Airy & Greensboro",
+  "Professional Popcorn Ceiling Removal in North Carolina",
+  "Top Skim Coating Solutions in Greensboro, NC",
+  "Quality Home Improvement in Mt. Airy",
+  "Local Painting and Restoration Experts in North Carolina",
+  "Greensboro's Affordable Popcorn Ceiling Removal Services",
+  "Skim Coating Services in Mt. Airy & Greensboro",
 ];
 
 export { TITLES, INDEX_KWS, DESCRIPTIONS };
